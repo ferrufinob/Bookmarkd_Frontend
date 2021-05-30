005a3ebf041ea92f5c5721a1e0e16af6bb6db0eb
Brenda Ferrufino <ferrufinob@gmail.com>
2021-05-30 17:32:13 -0400

    npm install redux/thunk/router/react-redux

--- src/index.js
+++ src/index.js
@@ -1,14 +1,32 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+
+import { createStore, compose, applyMiddleware } from "redux";
+import { reducer } from "/redux/reducer";
+import { Provider } from "react-redux";
+import thunk from "redux-thunk";
+import { BrowserRouter as Router } from "react-router-dom";
+
+const store = createStore(
+  reducer,
+  compose(
+    applyMiddleware(thunk),
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  )
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <Provider store={store}>
+        <App />
+      </Provider>
+    </Router>
   </React.StrictMode>,
-  document.getElementById('root')
+  document.getElementById("root")
 );
 
 // If you want to start measuring performance in your app, pass a function
